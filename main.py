1275729b157c51bd10a2486e865bc38f6b098462
DimOkGamer <dimokgamer@gmail.com>
2016-10-01 16:49:50 +0300

    Вывод пустого окна

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from methods import *
 from PyQt5 import QtCore
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+import sys
 import os
 
 def corrupt(in_file, out_file, corruptionMethod, settings, start_byte=0, end_byte=100, interval=3,
@@ -19,13 +20,13 @@ def corrupt(in_file, out_file, corruptionMethod, settings, start_byte=0, end_byt
 
 corrupt("in.txt", "out.txt", ReplaceMethod, {"byte_a":int("0x61", 16), "byte_b":int("0x67", 16)})
 
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#
-#     w = QWidget()
-#     w.resize(640, 480)
-#     w.move(300, 300)
-#     w.setWindowTitle('Simple')
-#     w.show()
-#
-#     sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(640, 480)
+    w.move(300, 300)
+    w.setWindowTitle('Simple')
+    w.show()
+
+    sys.exit(app.exec_())
