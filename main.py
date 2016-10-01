8b2162513c4ef254d2183996ceb73af52b3d867e
Арсений <screemarine@gmail.com>
2016-10-01 15:00:42 +0300

    Ухожу кушоть (на самом деле, не кушать, а смотреть фильм, НО ОБ ЭОТМ НИКОМУ)

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from methods import *
 from PyQt5 import QtCore
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-import sys
+import os
 
-def corrupt(in_file, out_file, corruptionMethod, overwrite=True):
+def corrupt(in_file, out_file, corruptionMethod, start_byte=0, end_byte=100, interval=3,
+            autoend=True, overwrite=False, **kwargs):
     temp_file = open(in_file, "rb")
     original = temp_file.read()
     temp_file.close()
-    corr = corruptionMethod(0, 100, 3)
-    if overwrite:
+    if autoend:
+        end_byte = len(original)
+    corr = corruptionMethod(start_byte, end_byte, interval)
+    if overwrite or not os.path.exists(out_file):
         temp_file = open(out_file, "wb")
         out_file = temp_file.write(corr.corrupt(original, byte_a=int("0x61", 16), byte_b=int("0x67", 16)))
         temp_file.close()
